6edb7f2e3ecd3e646d286ef4e2cf6406845ef1d2
caub <cyril.auburtin@gmail.com>
2017-10-12 14:54:26 +0200

    parser done, todo stringifier

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const romanSymbols = {
+const romanToIntMap = {
 	I:  1,
 	IV: 4,
 	V:  5,
@@ -13,10 +13,10 @@ const romanSymbols = {
 	CM: 900,
 	M:  1000
 };
-
-// const romanSymbolsRe = /^[IVXLCDM]/;
-const romanGroupRe = /[IVXLCDM]\1?\1?|IV|IX|XL|XC|CD|CM/g; // bit lazy to do that
-// const romanGroupRe = /I|I?V|I?X|X?L|X?C|C?D|C?M/; // ignore this one
+const intToRomanMap = {}; // invert romanToIntMap
+for (const key in romanToIntMap) {
+	intToRomanMap[romanToIntMap[key]] = key;
+}
 
 // comment: it's weird that Romans didn't write 990 XM instead of CMXC
 
@@ -24,23 +24,94 @@ class RomanNumber {
 	constructor(value) {
 		if (typeof value === 'number') {
 			this.value = value;
+			return;
 		}
 		if (!value || typeof value !== 'string') {
 			throw new TypeError('String or Integer value required');
 		}
+
+		this.value = 0; // intialize
+		const romanSymbolsRe = /^IV|IX|XL|XC|CD|CM|([IVXLCDM])\1*/g;
+		let m, prev = Infinity;
+		while (m = romanSymbolsRe.exec(value)) {
+			if (m[0].length > 3) {
+				throw new Error('invalid roman value');
+			}
+			const v = m[1] ? romanToIntMap[m[1]] * m[0].length : romanToIntMap[m[0]];
+
+			// ensure the value is less than the previous one
+			if (v >= prev) {
+				throw new Error('invalid roman value');
+			}
+			prev = v;
+			this.value += v;
+		}
 		
-		const matches = value.match(romanGroupRe);
-		if (matches) {
-			let v = 0;
-			// todo add up groups
-			this.value = v;
-		} else {
-			this.value = Number(value); // do we acept NaN or we have to throw? 
+		if (this.value === 0) { // try to parse as base10 Arabic number
+			this.value = Number(value);
+		}
+
+		if (isNaN(this.value)) {
+			throw new Error('invalid value');
+		}
+		if (!Number.isInteger(this.value)) {
+			throw new Error('invalid float value');
+		}
+		if (this.value < 1 || this.value > 3999) {
+			throw new RangeError('invalid range value');
 		}
 	}
 
+	toInt() {
+		return this.value;
+	}
+
 	toString() {
+		// 
 		throw new Error('toRoman todo');
 	}
 
 }
+
+module.exports = RomanNumber;
+
+RomanNumber.tests = function() {
+
+	const inputs = [ // [input, null for error or expected integer] // todo check which error
+		[null, null],
+		['', null],
+		[0, null], 
+		[1, 1],
+		[3, 3],
+		[4, 4],
+		[5, 5],
+		['I', 1],
+		['III', 3],
+		['IIII', null],
+		['IV', 4],
+		['V', 5],
+		[1968, 1968],
+		['1473', 1473],
+		[2999, 2999],
+		[3000, 3000],
+		[10000, 10000],
+		['CDXXIX', 429],
+		['CD1X', null],
+		['error', null],
+		['MCDLXXXII', 1482],
+		['MCMLXXX', 1980],
+		['MMMMCMXCIX', null],
+		['MMMMDMXCIX', null]
+	];
+
+	for (const [input, expected] of inputs) {
+		try {
+			const num = new RomanNumber(input);
+			console.assert(num.toInt() === expected);
+		} catch(e) {
+			console.assert(expected === null);
+		}
+	}
+};
+
+// RomanNumber.tests()
